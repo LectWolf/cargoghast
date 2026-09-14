9b5dfc820415c491fbaf90abe28aa1b328cfa612
LectWolf <67421358+LectWolf@users.noreply.github.com>
2026-09-15 03:47:58 +0800

    Ship one jar for 1.21.6-1.21.10 and reset the version to 1.0.0.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,10 +15,6 @@ plugins {
 stonecutter {
 	create(rootProject) {
 		version("1.21.6", "1.21.6").buildscript = "build.121.gradle"
-		version("1.21.7", "1.21.7").buildscript = "build.121.gradle"
-		version("1.21.8", "1.21.8").buildscript = "build.121.gradle"
-		version("1.21.9", "1.21.9").buildscript = "build.121.gradle"
-		version("1.21.10", "1.21.10").buildscript = "build.121.gradle"
 		version("1.21.11", "1.21.11").buildscript = "build.121.gradle"
 		version("26.1", "26.1")
 		version("26.2", "26.2")
